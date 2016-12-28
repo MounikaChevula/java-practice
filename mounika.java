72b361a084f698c11e04ee522378192a781875b4
MounikaChevula <mounikachevula6@gmail.com>
2016-12-27 18:58:13 -0800

    Add files via upload

---
+++ mounika.java
@@ -0,0 +1,20 @@
+/*string concate*/
+public class Concat {
+        public static void main(String args[]){
+               String str1 = "Hello";
+                String str2 = " World";
+                String str3 = str1 + str2;
+                System.out.println(str3);
+}
+}
+output:
+Hello World
+
+
+
+
+
+
+/*string length*/
+
+               
